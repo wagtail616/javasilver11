663a01f903091ca9ec780c3c2554b81771cf2b6b
勇介　齊藤 <wisteriaq9@gmail.com>
2024-01-16 17:21:52 +0900

    HelloWorld!

--- Hello.java
+++ Hello.java
@@ -2,7 +2,7 @@
  * Hell
  */
 public class Hello {
-public static void main(String[] args) {
-   System.out.println("Hello!"); 
-}
+    public static void main(String[] args) {
+        System.out.println("Hello!"); 
+    }
 }
